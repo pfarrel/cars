c404d61b00000750977ee3aef94fa5aa35280537
Padraig Farrell <pfarrel@tcd.ie>
2013-03-03 15:16:16 +0000

    Create Domain/Context and verify works

---
+++ Domain.Test/CreateTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Domain.Test
+{
+    [TestClass]
+    public class CreateTest
+    {
+        [TestMethod]
+        public void CreateWorks()
+        {
+            using (var context = new CarsContext())
+            {
+                Assert.AreEqual(0, context.Makes.ToList().Count());
+            }
+        }
+    }
+}
---
+++ Domain/CarsContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class CarsContext : DbContext
+    {
+        public DbSet<Make> Makes { get; set; }
+
+        public CarsContext() : base ("CarsContext")
+        {
+            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<CarsContext>());
+        }
+    }
+}
