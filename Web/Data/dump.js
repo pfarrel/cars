b70df47621a1e963195eb8d011d0b5e6cfe2649d
Padraig Farrell <pfarrel@tcd.ie>
2013-03-03 22:17:47 +0000

    Create web project with scatterplot of year/price

---
+++ Web/Data/dump.js
@@ -0,0 +1,142 @@
+[
+  {
+    "Make": "Fiat",
+    "Model": "Panda",
+    "Price": 1000,
+    "Year": 2010,
+    "Description": null
+  },
+  {
+    "Make": "Fiat",
+    "Model": "Panda",
+    "Price": 2000,
+    "Year": 2010,
+    "Description": null
+  },
+  {
+    "Make": "Fiat",
+    "Model": "Panda",
+    "Price": 3000,
+    "Year": 2011,
+    "Description": null
+  },
+  {
+    "Make": "Fiat",
+    "Model": "Panda",
+    "Price": 4000,
+    "Year": 2011,
+    "Description": null
+  },
+  {
+    "Make": "Fiat",
+    "Model": "Panda",
+    "Price": 5000,
+    "Year": 2013,
+    "Description": null
+  },
+  {
+    "Make": "Fiat",
+    "Model": "Doblo",
+    "Price": 1500,
+    "Year": 2007,
+    "Description": null
+  },
+  {
+    "Make": "Fiat",
+    "Model": "Doblo",
+    "Price": 2500,
+    "Year": 2008,
+    "Description": null
+  },
+  {
+    "Make": "Fiat",
+    "Model": "Doblo",
+    "Price": 3500,
+    "Year": 2009,
+    "Description": null
+  },
+  {
+    "Make": "Fiat",
+    "Model": "Doblo",
+    "Price": 4500,
+    "Year": 2010,
+    "Description": null
+  },
+  {
+    "Make": "Fiat",
+    "Model": "Doblo",
+    "Price": 5500,
+    "Year": 2011,
+    "Description": null
+  },
+  {
+    "Make": "Renault",
+    "Model": "Megane",
+    "Price": 750,
+    "Year": 2005,
+    "Description": null
+  },
+  {
+    "Make": "Renault",
+    "Model": "Megane",
+    "Price": 1750,
+    "Year": 2006,
+    "Description": null
+  },
+  {
+    "Make": "Renault",
+    "Model": "Megane",
+    "Price": 2750,
+    "Year": 2009,
+    "Description": null
+  },
+  {
+    "Make": "Renault",
+    "Model": "Megane",
+    "Price": 3750,
+    "Year": 2009,
+    "Description": null
+  },
+  {
+    "Make": "Renault",
+    "Model": "Megane",
+    "Price": 4750,
+    "Year": 2011,
+    "Description": null
+  },
+  {
+    "Make": "Renault",
+    "Model": "Clio",
+    "Price": 200,
+    "Year": 2003,
+    "Description": null
+  },
+  {
+    "Make": "Renault",
+    "Model": "Clio",
+    "Price": 1200,
+    "Year": 2004,
+    "Description": null
+  },
+  {
+    "Make": "Renault",
+    "Model": "Clio",
+    "Price": 2200,
+    "Year": 2007,
+    "Description": null
+  },
+  {
+    "Make": "Renault",
+    "Model": "Clio",
+    "Price": 3200,
+    "Year": 2005,
+    "Description": null
+  },
+  {
+    "Make": "Renault",
+    "Model": "Clio",
+    "Price": 4200,
+    "Year": 2006,
+    "Description": null
+  }
+]
