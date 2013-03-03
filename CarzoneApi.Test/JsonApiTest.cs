3c34329c17e81ae02094fd4e7c8d3d827ff0f478
Padraig Farrell <pfarrel@tcd.ie>
2013-03-03 13:37:42 +0000

    GetTotals working

---
+++ CarzoneApi.Test/JsonApiTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CarzoneApi;
+using System.Threading.Tasks;
+
+namespace CarzoneApi.Test
+{
+    [TestClass]
+    public class JsonApiTest
+    {
+        [TestMethod]
+        public async Task TestMethod1()
+        {
+            var api = new JsonApi();
+
+            var responseString = await api.GetTotals();
+            var a = 1;
+        }
+    }
+}
---
+++ CarzoneApi/JsonApi.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace CarzoneApi
+{
+    public class JsonApi
+    {
+        private const string BaseUrl = "http://www.carzone.ie/es-ie/search/";
+
+        public async Task<string> GetTotals()
+        {
+            var reqAddress = BaseUrl + "makeModelsJs?getResponseAsJson=true&requestor=cz";
+            HttpClient client = new HttpClient();
+
+            var response = await client.GetAsync(reqAddress);
+
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
