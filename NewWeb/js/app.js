b96d54c9e72a91532c49f2624a8c8cac3d2636e1
Padraig Farrell <pfarrel@tcd.ie>
2013-03-11 17:34:57 +0100

    Add location filtering by copypasta

--- NewWeb/js/app.js
+++ NewWeb/js/app.js
@@ -3,12 +3,15 @@ var App = Ember.Application.create();
 // Router
 App.Router.map(function () {
     this.resource('makemodels');
+    this.resource('locations');
 });
 
 App.ApplicationRoute = Ember.Route.extend({
     setupController: function () {
         this.controllerFor('makeModels').set('model', []);
         this.controllerFor('makeModels').set('allMakeModels', App.MakeModel.find());
+	this.controllerFor('locations').set('model', []);
+	this.controllerFor('locations').set('allLocations', App.Location.find());
     }
 });
 
@@ -31,12 +34,35 @@ App.MakeModelsController = Ember.ArrayController.extend({
     }
 });
 
+App.LocationsController = Ember.ArrayController.extend({
+    search: '',
+
+    searchedContent: function () {
+        var regexp = new RegExp(this.get('search'), 'i');
+        var controller = this;
+        return this.get('allLocations').filter(function(item) {
+            return !controller.contains(item) && regexp.test(item.get('name'));
+        });
+    }.property('search', 'model.@each', 'allLocations.@each'),    // Depend on allMakeModels so that this is computed on initial load
+
+    addLocation: function (location) {
+        this.addObject(location);
+    },
+    removeLocation: function (location) {
+        this.removeObject(location);
+    }
+});
+
 App.Store = DS.Store.extend({
     revision: 11,
     adapter: 'DS.FixtureAdapter'
 });
 
 App.MakeModel = DS.Model.extend({
+    name: DS.attr('string')
+});
+
+App.Location = DS.Model.extend({
     name: DS.attr('string')
 });
 
@@ -47,4 +73,13 @@ App.MakeModel.FIXTURES = [
     { id: 4, name: 'Peugot 206' },
     { id: 5, name: 'Volkswagen Touran' },
     { id: 6, name: 'Toyota Corolla' }
+];
+
+App.Location.FIXTURES = [
+    { id: 1, name: 'Dublin' },
+    { id: 2, name: 'Wicklow' },
+    { id: 3, name: 'Cork' },
+    { id: 4, name: 'Galway' },
+    { id: 5, name: 'Waterford' },
+    { id: 6, name: 'Limerick' }
 ];
