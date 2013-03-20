ae4ebcd0b3acc30812885e0d0c3c5a3b529c947c
Padraig Farrell <pfarrel@tcd.ie>
2013-03-20 22:56:56 +0000

    Add DoneDealApi

---
+++ Scraper.Test/DoneDealApiTest.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Scraper.CarsIreland;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Scraper.Test
+{
+    [TestClass]
+    public class DoneDealApiTest
+    {
+        DoneDealApi api = new DoneDealApi();
+
+        [TestMethod]
+        public void GetSearchResultsString_LooksRight()
+        {
+            var responseString = api.GetSearchResultsString(0);
+            Assert.IsTrue(responseString.Contains("sectionDisplay\":\"Cars\""));
+        }
+
+        [TestMethod]
+        public void GetSearchResults_Deserializes()
+        {
+            var response = api.GetSearchResults(0);
+            Assert.IsTrue(response.TotalCount > 10000);
+            Assert.IsTrue(response.Ads.Count() == 15);
+        }
+
+        [TestMethod]
+        public void GetListingString_LooksRight()
+        {
+            var responseString = api.GetListingString(4705634);
+            Assert.IsTrue(responseString.Contains("mileage_metric"));
+        }
+
+        [TestMethod]
+        public void GetListing_DeserializesAllValues()
+        {
+            var listing = api.GetListing(4705634);
+
+            Assert.IsTrue(!string.IsNullOrEmpty(listing.County));
+            Assert.IsTrue(!string.IsNullOrEmpty(listing.Description));
+            Assert.AreNotEqual(0, listing.Price);
+            Assert.IsTrue(!string.IsNullOrEmpty(listing.Make));
+            Assert.IsTrue(!string.IsNullOrEmpty(listing.Model));
+            Assert.IsTrue(!string.IsNullOrEmpty(listing.Mileage_Metric));
+            Assert.AreNotEqual(0, listing.Year);
+            Assert.AreNotEqual(0, listing.Mileage);
+            Assert.IsTrue(!string.IsNullOrEmpty(listing.FuelType));
+        }
+
+        [TestMethod]
+        public void GetXDetails_Gets()
+        {
+            var listings = api.GetXDetails(1);
+            Assert.IsTrue(listings.Count() == 15);
+        }
+    }
+}
---
+++ Scraper/DoneDealApi.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Scraper
+{
+    public class DoneDealApi : ApiBase
+    {
+        private const string BaseUrl = "http://api.donedeal.ie/api/";
+
+        public DoneDealApi() : base(BaseUrl)
+        {
+        }
+
+        public string GetSearchResultsString(int page)
+        {
+            var payload = JsonConvert.SerializeObject(new { start = page, source = new[] { "private", "trade" } });
+            var response = client.PostAsync("find/cars/for-sale/Ireland/", new StringContent(payload, Encoding.UTF8, "application/json")).Result;
+            return response.Content.ReadAsStringAsync().Result;
+        }
+
+        public DoneDealSearchResponse GetSearchResults(int page)
+        {
+            var responseString = GetSearchResultsString(page);
+            var response = JsonConvert.DeserializeObject<DoneDealSearchResponse>(responseString);
+            return response;
+        }
+
+        public string GetListingString(long listingId)
+        {
+            var response = MakeRequestSynchronous("view/ad/" + listingId);
+            return response;
+        }
+
+        public DoneDealDetails GetListing(long listingId)
+        {
+            var response = GetListingString(listingId);
+            return DoneDealDetails.FromJsonString(response);
+        }
+
+        public IEnumerable<DoneDealDetails> GetXDetails(int count)
+        {
+            int numReqs = ((count - 1) / 15) + 1;
+            for (int i = 0; i < numReqs; i++)
+            {
+                var searchResponse = GetSearchResults(i);
+                foreach (var listing in searchResponse.Ads)
+                {
+                    yield return GetListing(listing.Id);
+                }
+            }
+        }
+    }
+
+    public class DoneDealSearchResponse
+    {
+        public int TotalCount { get; set; }
+        public int PrivateCount { get; set; }
+        public int TradeCount { get; set; }
+
+        public List<DoneDealListing> Ads { get; set; }
+    }
+
+    public class DoneDealListing
+    {
+        public long Id { get; set; }
+    }
+
+    [JsonObject(MemberSerialization.OptIn)]
+    public class DoneDealDetails
+    {
+        [JsonProperty]
+        public string County { get; set; }
+        [JsonProperty]
+        public string Description { get; set; }
+        [JsonProperty]
+        public int Price { get; set; }
+        [JsonProperty]
+        public List<DDAttrib> Attributes { get; set; }
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public int Mileage { get; set; }
+        public string Mileage_Metric { get; set; }
+        public Decimal EngineSize { get; set; }
+        public string FuelType { get; set; }
+
+        public static DoneDealDetails FromJsonString(string json)
+        {
+            var jObject = JObject.Parse(json);
+            var inner = jObject["ad"];
+
+            var listing = JsonConvert.DeserializeObject<DoneDealDetails>(inner.ToString());
+            listing.Make = listing.Attributes.Single(a => a.Name == "make").Value;
+            listing.Model = listing.Attributes.Single(a => a.Name == "model").Value;
+            listing.Year = Int32.Parse(listing.Attributes.Single(a => a.Name == "year").Value);
+            //listing.Mileage_Metric = listing.Attributes.Single(a => a.Name == "mileage_metric").Value;
+            //listing.EngineSize = Decimal.Parse(listing.Attributes.Single(a => a.Name == "engine").Value);
+            //listing.FuelType = listing.Attributes.Single(a => a.Name == "fuelType").Value;
+
+            var mileage = listing.Attributes.SingleOrDefault(a => a.Name == "mileage");
+            listing.Mileage = (mileage != null) ? Int32.Parse(mileage.Value) : -1;
+
+            return listing;
+        }
+
+        public class DDAttrib
+        {
+            public string Name { get; set; }
+            public string Value { get; set; }
+        }
+    }
+}
